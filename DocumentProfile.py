541eaf0dfa0c0602cca4352449316971a9496f1d
livvy94 <haxxor199x@gmail.com>
2023-05-10 01:23:50 -0400

    First steps

---
+++ DocumentProfile.py
@@ -0,0 +1,24 @@
+class DocumentProfile:
+    def __init__(self, ein, returntype, state, subcode, subdate, taxyear, taxpayername, totalassets, zipcode):
+        self.ein = ein
+        self.returntype = returntype
+        self.state = state
+        self.subcode = subcode
+        self.subdate = subdate
+        self.taxyear = taxyear
+        self.taxpayername = taxpayername
+        self.totalassets = totalassets
+        self.zipcode = zipcode
+
+def __str__(self):
+    result = f"==== Document Profile for {self.ein}  ====\r"
+    # result += f"EIN: {self.ein}\r"
+    result += f"Return Type: {self.returntype}\r"
+    result += f"State: {self.state}\r"
+    result += f"Sub Code: {self.subcode}\r"
+    result += f"Sub Date: {self.subdate}\r"
+    result += f"Tax Year: {self.taxyear}\r"
+    result += f"Taxpayer Name: {self.taxpayername}\r"
+    result += f"Total Assets: {self.totalassets}\r"
+    result += f"ZIP Code: {self.zipcode}\r"
+    return result
---
+++ extract_metadata.py
@@ -0,0 +1,54 @@
+import DocumentProfile
+
+# TODO: Download more disks and figure out the folder structure so users can specify the location of the disk and not have to hunt down the specific file
+filename = r"C:\Users\vince\Dropbox\00 Grad School\05 Summer 2023\Test disk\1998 Jul - Dec; Private Foundation Returns for ALABAMA; pfal000s001\PFAL000S\DATA000\00001.PRO"
+START_CODE = '0023000A'
+END_CODES = ['000701240006', '000701250003', '000701260002', '000701270008', '000701280007', '000701290013', '0007012A0007', '0007012B0006']
+    # Note to self: Each of these start with [00 07 01]
+    # followed by three more bytes. Find just that?
+END_ALL_CODE = '000305190000'
+
+
+# Convert these codes into bytes
+start_code = bytes.fromhex(START_CODE)
+end_codes = [bytes.fromhex(code) for code in END_CODES]
+end_all_code = bytes.fromhex(END_ALL_CODE)
+
+def main():
+  with open(filename, 'rb') as f:
+      data = f.read()
+
+  # Find each record's position in the binary file
+  all_start_offsets = []
+  pos = data.find(start_code) # Find the first instance of the start code
+  while pos != -1: # Loop until it can't find any more
+    all_start_offsets.append(pos)
+    pos = data.find(start_code, pos + 1) # Look for another one!
+
+  for offset in all_start_offsets:
+    start_pos = offset
+    for code in end_codes:
+        end_pos = data.find(code, offset)
+        if end_pos != -1:
+            result = data[start_pos:end_pos].decode('unicode_escape')
+            print(result)
+            start_pos = start_pos + len(result) + len(code)
+        else:
+            break
+
+# TODO: Make a class that it puts all the data into instead of printing it.
+# TODO: Give that class an overrided version of string that makes it print it all pretty
+# TODO: Turn stuff into methods + a main()
+# Idea for a better search method:
+#  Get offset of start_code
+#  Get offset of end_all_code
+#  find() the next instance of [00 07 01 * * *]
+#  Use the DocumentProfile class.
+#  If said instance is after end_all_code, the record is complete
+#  and it's time to search for the next instance of start_code,
+#  using the offset of end_all_code as a starting point.
+
+#def find_new_record(data):
+#    return data.find(start_code) + len(start_code)
+
+main()
