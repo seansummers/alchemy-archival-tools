d7d2a80d69983bb9a134a01242d5a7a1a8c5522a
livvy94 <haxxor199x@gmail.com>
2023-06-26 20:00:59 -0500

    Finish multiple-file functionality, make json filenames descriptive

--- extract_metadata.py
+++ extract_metadata.py
@@ -1,19 +1,16 @@
 from DocumentProfile import DocumentProfile
-from secrets import filenames
-import os.path
+from secrets import list_of_files
+from pathlib import Path
 
 def main():
-    for filename in filenames:
-        # Get the name of the CD-ROM we're currently looking at
-        # The path looks like this:
-        # C:\Users\olivia\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005\Mss005_[four-digit index number]\[THE DIRECTORY NAME WE WANT]\PFAL000S\DATA000\00001.PRO
-        current_cdrom_title = os.path.split(os.path.dirname(filename))[-3]
+    for filename in list_of_files:
+        current_title = get_cdrom_name(filename)
 
         with open(filename, 'rb') as f:
             data = f.read() # This "data" variable is where the entire file's contents is going to be stored
 
         records = extract(data)
-        save_metadata(records, current_cdrom_title) # Save to a file
+        save_json_file(records, current_title)
 
 ########################################################################
 
@@ -54,21 +51,22 @@ def extract(data):
         temp = DocumentProfile(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8])
         records.append(temp)
         index = index + 1
-    
     return records
 
 
 # Helper methods!
+def get_cdrom_name(filename):
+    return Path(filename).resolve().parents[2].name # This takes the whole filepath and returns just the name of the folder.
+
 def get_text(data, start_offset, end_offset):
     return data[start_offset:end_offset].decode('unicode_escape')
 
 def convert_to_bytes(input):
     return bytes.fromhex(input) # Converts a string that contains a hex number into that number. Simple but this looks cleaner.
 
-def save_metadata(records, directory_name):
+def save_json_file(records, directory_name):
     result = ""
-    # Save as JSON
-    with open(f"metadata_{directory_name}.json", "a") as write_file:
+    with open(f"metadata for {directory_name}.json", "a") as write_file:
         for record in records:
             result = record.toJSON() + "\r\n"
             write_file.write(result)
@@ -82,10 +80,3 @@ if __name__ == "__main__":
 
 # TODO: Download more disks and figure out the folder structure so users can specify
 #       the location of the disk and not have to hunt down the specific file
-
-# TODO: Alter save_metadata to use the directory name when saving the .JSON file
-# Ideas: Maybe use a text file that's full of paths for it to rip through, and somehow gleam it from that?
-# How many of these CD-ROMs can I download onto my computer at once, though?
-# Does that matter? A user (or me) might want to do small batches.
-# File structure is entirely in OneDrive...
-# C:\Users\olivia\Indiana University\O365-[Sec] IN-ULIB-BornDigital - Mss005\Mss005_[four-digit index number]\[THE DIRECTORY NAME WE WANT]\PFAL000S\DATA000\00001.PRO
--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
 
-filenames = [
+list_of_files = [
     r"Delete this and put your filepaths here!",
     r"Delete this and put your filepaths here!",
     r"Delete this and put your filepaths here!",
-    r"Delete this and put your filepaths here!"
+    r"Delete this and put your filepaths here!",
 ]
